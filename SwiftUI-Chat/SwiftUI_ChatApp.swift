4181dd47b77fc5f7422228f76cdea979e4e7ec52
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2022-12-09 12:09:51 +0300

    Initial Commit

---
+++ SwiftUI-Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI-Chat
+//
+//  Created by ed on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
