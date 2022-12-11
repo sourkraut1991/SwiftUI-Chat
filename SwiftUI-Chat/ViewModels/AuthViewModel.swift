f8b48c1f6b0b38d4bbf765a0450626895c76eb36
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2022-12-11 07:47:40 +0300

    - conditionally showed login sequence

--- SwiftUI-Chat/ViewModels/AuthViewModel.swift
+++ SwiftUI-Chat/ViewModels/AuthViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class AuthViewModel {
+    static func isUserLoggedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    static func getLoggedInUserId() -> String {
+        return Auth.auth().currentUser?.uid ?? ""
+    }
+    static func logout() {
+        try? Auth.auth().signOut()
+    }
+}
