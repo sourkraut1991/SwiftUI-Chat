9c0b06c9ce318b9f7036f0213029e3bf8d7a6eb3
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2022-12-11 15:03:25 +0300

    Fixed contact retrieval

--- SwiftUI-Chat/Services/DatabaseService.swift
+++ SwiftUI-Chat/Services/DatabaseService.swift
@@ -11,67 +11,69 @@ import Firebase
 
 class DatabaseService {
     
-    func getPlatformUsers(localContacts: [CNContact], completion: @escaping([User]) -> Void) {
+    func getPlatformUsers(localContacts: [CNContact], completion: @escaping ([User]) -> Void) {
         
         // The array where we're storing fetched platform users
         var platformUsers = [User]()
         
-        
         // Construct an array of string phone numbers to look up
-        var lookupPhoneNumbers =   localContacts.map { contact in
+        var lookupPhoneNumbers = localContacts.map { contact in
             
             // Turn the contact into a phone number as a string
             return TextHelper.sanitizePhoneNumber(contact.phoneNumbers.first?.value.stringValue ?? "")
-            
-            
         }
         
+        // Make sure that there are lookup numbers
         guard lookupPhoneNumbers.count > 0 else {
+            
+            // Callback
             completion(platformUsers)
             return
         }
         
+        // Query the database for these phone numbers
+        let db = Firestore.firestore()
         
-        
-        // Perform queries hwile we still have phone numbers to look up
+        // Perform queries while we still have phone numbers to look up
         while !lookupPhoneNumbers.isEmpty {
-            
-            // Query the database for these phone numbers
-            let db = Firestore.firestore()
-            
-            // Get the first 10 phonenumbers to look up
+        
+            // Get the first < 10 phone numbers to look up
             let tenPhoneNumbers = Array(lookupPhoneNumbers.prefix(10))
             
-            // Remove the < 10 that we're lookign up
+            // Remove the < 10 that we're looking up
             lookupPhoneNumbers = Array(lookupPhoneNumbers.dropFirst(10))
             
-            // look up the first 10
-            let query = db.collection("users").whereField("phone", in: lookupPhoneNumbers)
-            
+            // Look up the first 10
+            let query = db.collection("users").whereField("phone", in: tenPhoneNumbers)
+        
             // Retrieve the users that are on the platform
             query.getDocuments { snapshot, error in
                 
-                // check for errors
+                // Check for errors
                 if error == nil && snapshot != nil {
                     
                     // For each doc that was fetched, create a user
                     for doc in snapshot!.documents {
                         
                         if let user = try? doc.data(as: User.self) {
+                            
                             // Append to the platform users array
                             platformUsers.append(user)
                         }
                     }
+                    
                     // Check if we have anymore phone numbers to look up
-                    // If not, we can call the copmpletion block and we're done
+                    // If not, we can call the completion block and we're done
                     if lookupPhoneNumbers.isEmpty {
                         // Return these users
                         completion(platformUsers)
                     }
-                    
                 }
             }
         }
         
+        
+        
     }
+    
 }
